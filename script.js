a3d7c439cbbe64db6e3d91fffc986c2973de1c94
hendhendy <hendy.maxv@gmail.com>
2021-05-16 15:59:40 +0700

    DONE!

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+function fizzBuzz(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) console.log('Halo FizzBuzz!');
+    else if (i % 5 === 0) console.log('Halo Buzz');
+    else if (i % 3 === 0) console.log('Halo Fizz');
+    else console.log(i);;
+  }
+}
+
+fizzBuzz(50);
+
