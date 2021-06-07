f2db59d460cb21a0fdeb02fca93ee772da1db040
Hendy <hendy.maxv@gmail.com>
2021-06-07 15:34:51 +0700

    Enter

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 function fizzBuzz(num) {
   for (let i = 1; i <= num; i++) {
     if (i % 15 === 0) console.log('Halo FizzBuzz!');
-    else if (i % 5 === 0) console.log('Halo Buzz');
     else if (i % 3 === 0) console.log('Halo Fizz');
+    else if (i % 5 === 0) console.log('Halo Buzz');
     else console.log(i);;
   }
 }
